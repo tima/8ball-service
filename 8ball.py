cdf29975252a151bb83786e7827cf5610d19bc89
Timothy Appnel <tima@ansible.com>
2016-06-20 17:42:33 -0400

    Initial commit of basic basic script.

---
+++ 8ball.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import random
+
+answers = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful",
+]
+
+def shake_8ball():
+        return answers[random.randint(0, len(answers))-1]
+
+if __name__ == "__main__":
+        print shake_8ball()
